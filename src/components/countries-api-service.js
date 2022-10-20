f3121678d81d2e4362429e31a8de3b36c121c6bc
Alexey-Kushnir <al7787@gmail.com>
2022-10-21 00:41:03 +0300

    Update countries-api-service.js

--- src/components/countries-api-service.js
+++ src/components/countries-api-service.js
@@ -13,15 +13,17 @@ export default class CountriesApiService {
     if (this.searchQuery === '') {
       return;
     }
-    return fetch(url).then(response => {
-      if (!response.ok) {
-        // throw new Error(
-        //   Notiflix.Notify.failure('Oops, there is no country with that name')
-        // );
-        Notiflix.Notify.failure('Oops, there is no country with that name');
-      }
-      return response.json();
-    });
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          // throw new Error(
+          //   Notiflix.Notify.failure('Oops, there is no country with that name')
+          // );
+          Notiflix.Notify.failure('Oops, there is no country with that name');
+        }
+        return response.json();
+      })
+      .catch(error => console.log(error));
   }
 
   get query() {
